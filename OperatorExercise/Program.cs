b4be46b4ac075fc0dcc68484eed4aff35325559b
DanielJSturdivant <DanielJSturdivant@gmail.com>
2023-07-06 14:18:12 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exersice 1
+
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a} divided by {b} is {quotient} with a remainder of {remainder}." );
+            }
+
+            AreaOfCircle();
+
+
+
+        }
+
+        //Exercise 2
+
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * radius * radius;
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
         }
     }
 }
